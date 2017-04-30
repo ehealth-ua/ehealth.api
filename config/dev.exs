957932e55e9ff598680d46b09e3e4fe4d4ae6b9f
Pavel Vesnin <pavel.vesnin.v@gmail.com>
2017-04-30 16:01:45 +0300

    improved dev debug config (#97)

--- config/dev.exs
+++ config/dev.exs
@@ -11,13 +11,15 @@ use Mix.Config
 # with brunch.io to recompile .js and .css sources.
 config :ehealth, EHealth.Web.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: []
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console,
+  format: "[$level] $message\n",
+  level: :debug
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
