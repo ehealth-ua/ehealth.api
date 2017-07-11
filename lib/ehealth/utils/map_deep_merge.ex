649940bb691c4cdde975fbbc48761d3f0bdcfdde
gmile <iamexile@gmail.com>
2017-07-11 10:34:36 +0300

    Fix travis build

--- lib/ehealth/utils/map_deep_merge.ex
+++ lib/ehealth/utils/map_deep_merge.ex
@@ -7,6 +7,6 @@ defmodule EHealth.Utils.MapDeepMerge do
     Map.merge(left, right, &deep_resolve/3)
   end
 
-  defp deep_resolve(_key, left = %{}, right = %{}), do: merge(left, right)
+  defp deep_resolve(_key, %{} = left, %{} = right), do: merge(left, right)
   defp deep_resolve(_key, _left, right), do: right
 end
