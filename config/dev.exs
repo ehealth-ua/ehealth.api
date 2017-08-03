3463583f83f7d3b0c6d0378a763ad0190ffbec1a
gmile <iamexile@gmail.com>
2017-08-03 15:13:41 +0300

    Allow any port in dev

--- config/dev.exs
+++ config/dev.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :ehealth, EHealth.Web.Endpoint,
-  http: [port: 4000],
+  http: [port: {:system, "PORT", 4000}],
   debug_errors: false,
   code_reloader: true,
   check_origin: false,
