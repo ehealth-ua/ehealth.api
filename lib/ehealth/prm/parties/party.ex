4e3ac919e7430b71ebb35b6eac56964352d51a32
Pavel Vesnin <pavel.vesnin.v@gmail.com>
2017-08-07 12:39:43 +0300

    Party and PartyUsers schema (#783)

---
+++ lib/ehealth/prm/parties/party.ex
@@ -0,0 +1,25 @@
+defmodule EHealth.PRM.Parties.Party do
+  @moduledoc false
+
+  use Ecto.Schema
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "parties" do
+    field :first_name, :string
+    field :last_name, :string
+    field :second_name, :string
+    field :birth_date, :date
+    field :gender, :string
+    field :tax_id, :string
+    field :inserted_by, Ecto.UUID
+    field :updated_by, Ecto.UUID
+
+    embeds_many :phones, EHealth.PRM.Meta.Phone, on_replace: :delete
+    embeds_many :documents, EHealth.PRM.Meta.Document, on_replace: :delete
+
+    has_many :users, EHealth.PRM.Parties.PartyUser
+
+    timestamps()
+  end
+end
---
+++ lib/ehealth/prm/parties/party_user.ex
@@ -0,0 +1,14 @@
+defmodule EHealth.PRM.Parties.PartyUser do
+  @moduledoc false
+
+  use Ecto.Schema
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  schema "party_users" do
+    field :user_id, Ecto.UUID
+
+    belongs_to :party, EHealth.PRM.Parties.Party, type: Ecto.UUID
+
+    timestamps()
+  end
+end
