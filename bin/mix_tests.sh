8b46614957139fe85b41bc28e3a6523a0b0d95f1
vsirius <siriusgti@gmail.com>
2017-08-30 18:30:31 +0300

    Chenged travis.yaml

--- bin/mix_tests.sh
+++ bin/mix_tests.sh
@@ -1,5 +1,26 @@
+#!bin/bash
 cd $TRAVIS_BUILD_DIR
 mix test --exclude pending --trace 
+     if [ "$?" -eq 0 ]; then
+     	 exit 0;
+     else 
+     	exit 1;
+     fi;
 mix coveralls.travis --exclude pending 
+     if [ "$?" -eq 0 ]; then
+     	 exit 0;
+     else 
+     	exit 1;
+     fi;
 mix credo --strict
+     if [ "$?" -eq 0 ]; then
+     	 exit 0;
+     else 
+     	exit 1;
+     fi;
 mix dogma 
+     if [ "$?" -eq 0 ]; then
+     	 exit 0;
+     else 
+     	exit 1;
+     fi;
