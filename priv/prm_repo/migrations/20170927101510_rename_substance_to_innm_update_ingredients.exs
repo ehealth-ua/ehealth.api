f9bb166d898eb385a0837fb0fde5c39078fa785d
PavelVesnin <pavel.vesnin.v@gmail.com>
2017-10-03 12:57:26 +0300

    INNM sctid could be NULL

--- priv/prm_repo/migrations/20170927101510_rename_substance_to_innm_update_ingredients.exs
+++ priv/prm_repo/migrations/20170927101510_rename_substance_to_innm_update_ingredients.exs
@@ -7,7 +7,7 @@ defmodule EHealth.PRMRepo.Migrations.RenameSubstancesToINNMUpdateIngredients do
 
     create table(:innms, primary_key: false) do
       add :id, :uuid, primary_key: true
-      add :sctid, :string, null: false
+      add :sctid, :string, null: true
       add :name, :string, null: false
       add :name_original, :string, null: false
       add :is_active, :boolean, default: false, null: false
