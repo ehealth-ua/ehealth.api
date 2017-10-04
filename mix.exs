050baceb41f0e460e02328ad5f7251af8106711b
gmile <iamexile@gmail.com>
2017-10-04 13:17:37 +0300

    Bump plug

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule EHealth.Mixfile do
      {:timex, ">= 3.1.15"},
      {:logger_json, "~> 0.5.0"},
      {:poison, "~> 3.1"},
-     {:plug, "~> 1.3.5"},
+     {:plug, "~> 1.4"},
      {:cowboy, "~> 1.1"},
      {:httpoison, "~> 0.12"},
      {:hackney, "== 1.8.0", override: true}, # fix for https://github.com/edgurgel/httpoison/issues/264
