33b2a5dc19b15af02410e12601c5c450fd8b1852
EdwardVoloshin <30432256+EdwardVoloshin@users.noreply.github.com>
2017-10-06 15:27:15 +0300

    rename innm_schema.json [ci skip] (#1200)
    * Create new_innm_dosage_schema_response [ci skip]
    
    * delete new_innm_schema.json [ci skip]
    
    * Create innm_create_request.json
    
    * [ci skip] innm/innm_create_request.json
    
    * Delete new_innm_schema_response.json
    
    * Create innm_create_response.json
    
    * Rename specs/json_schemas/new_innm_dosage_schema_response.json to specs/json_schemas/innm_dosage/innm_dosage_create_response.json
    
    * Rename specs/json_schemas/new_innm_dosage_schema.json to specs/json_schemas/innm_dosage/innm_dosage_create_request.json
    
    * Rename specs/json_schemas/new_medication_schema.json to specs/json_schemas/medication/medication_create_request.json
    
    * Rename specs/json_schemas/new_medication_request_request_schema.json to specs/json_schemas/medication_request_request/medication_request_request_create_request.json
    
    * Rename specs/json_schemas/SignMedicationRequestRequest.json to specs/json_schemas/medication_request_request/medication_request_request_sign_request.json
    
    * Rename specs/json_schemas/PostPreQualifyMedicationRequestRequest.json to specs/json_schemas/medication_request_request/medication_request_request_prequalify_request.json
    
    * Rename specs/json_schemas/new_medication_dispense_request_schema.json to specs/json_schemas/medication_dispense/medication_dispense_create_request.json
    
    * Update json_schema.ex
    
    * typo, codestyle

--- lib/ehealth/validators/json_schema.ex
+++ lib/ehealth/validators/json_schema.ex
@@ -6,17 +6,18 @@ defmodule EHealth.Validators.JsonSchema do
   use JValid
   alias EHealth.Validators.SchemaMapper
 
-  use_schema :medication_request_request, "specs/json_schemas/new_medication_request_request_schema.json"
-  use_schema :medication_dispense, "specs/json_schemas/new_medication_dispense_request_schema.json"
+  use_schema :medication_request_request,
+             "specs/json_schemas/medication_request_request/medication_request_request_create_request.json"
+  use_schema :medication_dispense, "specs/json_schemas/medication_dispense/medication_dispense_create_request.json"
   use_schema :declaration_request, "specs/json_schemas/declaration_request_schema.json"
   use_schema :division, "specs/json_schemas/division_schema.json"
   use_schema :employee_request, "specs/json_schemas/new_employee_request_schema.json"
   use_schema :employee_doctor, "specs/json_schemas/employee_doctor_schema.json"
   use_schema :employee_pharmacist, "specs/json_schemas/employee_pharmacist_schema.json"
   use_schema :legal_entity, "specs/json_schemas/new_legal_entity_schema.json"
-  use_schema :innm, "specs/json_schemas/new_innm_schema.json"
-  use_schema :medication, "specs/json_schemas/new_medication_schema.json"
-  use_schema :innm_dosage, "specs/json_schemas/new_innm_dosage_schema.json"
+  use_schema :innm, "specs/json_schemas/innm/innm_create_request.json"
+  use_schema :medication, "specs/json_schemas/medication/medication_create_request.json"
+  use_schema :innm_dosage, "specs/json_schemas/innm_dosage/innm_dosage_create_request.json"
   use_schema :program_medication, "specs/json_schemas/program_medications_create_request.json"
   use_schema :program_medication_update, "specs/json_schemas/program_medications_update_request.json"
 
