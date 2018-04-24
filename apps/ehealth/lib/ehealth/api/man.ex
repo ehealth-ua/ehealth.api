9ff27dee15a008df6dba4167d7f7662cab953a21
gmile <iamexile@gmail.com>
2018-04-24 17:09:20 +0300

    [minor] Hotfix man interaction

--- apps/ehealth/lib/ehealth/api/man.ex
+++ apps/ehealth/lib/ehealth/api/man.ex
@@ -3,11 +3,55 @@ defmodule EHealth.API.Man do
   Man API client
   """
 
-  use EHealth.API.Helpers.MicroserviceBase
+  use HTTPoison.Base
+  use Confex, otp_app: :ehealth
+  require Logger
 
   @behaviour EHealth.API.ManBehaviour
 
+  @filter_headers ["content-length", "Content-Length", "api-key", "authorization"]
+
+  def process_url(url), do: config()[:endpoint] <> url
+
   def render_template(id, data, headers \\ []) do
-    post!("/templates/#{id}/actions/render", Poison.encode!(data), headers)
+    path = "/templates/#{id}/actions/render"
+
+    headers =
+      headers
+      |> Keyword.drop(@filter_headers)
+      |> Kernel.++([{"Content-Type", "application/json"}])
+
+    Logger.info(fn ->
+      Poison.encode!(%{
+        "log_type" => "microservice_request",
+        "microservice" => config()[:endpoint],
+        "action" => "POST",
+        "path" => Enum.join([config()[:endpoint], path]),
+        "request_id" => Logger.metadata()[:request_id],
+        "body" => data,
+        "headers" => headers
+      })
+    end)
+
+    path
+    |> post!(Poison.encode!(data), headers, config()[:hackney_options])
+    |> process_template()
+  end
+
+  defp process_template(%HTTPoison.Response{body: body, status_code: 200}) do
+    {:ok, body}
+  end
+
+  defp process_template(%HTTPoison.Response{body: body}) do
+    Logger.error(fn ->
+      Poison.encode!(%{
+        "log_type" => "microservice_response",
+        "microservice" => config()[:endpoint],
+        "response" => body,
+        "request_id" => Logger.metadata()[:request_id]
+      })
+    end)
+
+    {:error, body}
   end
 end
