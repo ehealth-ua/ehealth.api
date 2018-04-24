01d05721da174c725a8bd992dee22d87805694b7
gmile <iamexile@gmail.com>
2018-04-24 17:09:20 +0300

    [minor] Hotfix man interaction

--- apps/ehealth/lib/ehealth/api/man.ex
+++ apps/ehealth/lib/ehealth/api/man.ex
@@ -5,16 +5,36 @@ defmodule EHealth.API.Man do
 
   use HTTPoison.Base
   use Confex, otp_app: :ehealth
-  use EHealth.API.Helpers.MicroserviceBase
   require Logger
 
   @behaviour EHealth.API.ManBehaviour
 
+  @filter_headers ["content-length", "Content-Length", "api-key", "authorization"]
+
+  def process_url(url), do: config()[:endpoint] <> url
+
   def render_template(id, data, headers \\ []) do
     path = "/templates/#{id}/actions/render"
 
+    headers =
+      headers
+      |> Keyword.drop(@filter_headers)
+      |> Kernel.++([{"Content-Type", "application/json"}])
+
+    Logger.info(fn ->
+      Poison.encode!(%{
+        "log_type" => "microservice_request",
+        "microservice" => config()[:endpoint],
+        "action" => "POST",
+        "path" => Enum.join([config()[:endpoint], path]),
+        "request_id" => Logger.metadata()[:request_id],
+        "body" => data,
+        "headers" => headers
+      })
+    end)
+
     path
-    |> post!(Poison.encode!(data), headers)
+    |> post!(Poison.encode!(data), headers, config()[:hackney_options])
     |> process_template()
   end
 
