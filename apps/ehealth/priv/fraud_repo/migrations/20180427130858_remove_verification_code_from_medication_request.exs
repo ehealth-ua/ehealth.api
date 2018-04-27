586a9bea518bffdc7c86e3521a303945a6ed1199
Pavel Vesnin <pavel.vesnin.v@gmail.com>
2018-04-27 16:19:44 +0300

    [patch] remove verification_code from medication_request in FraudDB (#2247)

---
+++ apps/ehealth/priv/fraud_repo/migrations/20180427130858_remove_verification_code_from_medication_request.exs
@@ -0,0 +1,9 @@
+defmodule EHealth.FraudRepo.Migrations.RemoveVerificationCodeFromMedicationRequest do
+  use Ecto.Migration
+
+  def change do
+    alter table(:medication_requests, primary_key: false) do
+      remove(:verification_code)
+    end
+  end
+end
