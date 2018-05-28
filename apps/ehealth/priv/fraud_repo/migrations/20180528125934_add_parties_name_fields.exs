e7f6f7a551ba45763987b9930ce9d5f9f8436054
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-05-28 16:13:04 +0300

    [minor] add party name fields to fraud db (#2336)

---
+++ apps/ehealth/priv/fraud_repo/migrations/20180528125934_add_parties_name_fields.exs
@@ -0,0 +1,13 @@
+defmodule EHealth.FraudRepo.Migrations.AddPartiesNameFields do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  def change do
+    alter table(:parties) do
+      add(:first_name, :string)
+      add(:last_name, :string)
+      add(:second_name, :string)
+    end
+  end
+end
