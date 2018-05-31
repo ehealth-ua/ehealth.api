730c1f7eb1c446793c0bbeacc3381a121cbfe8b6
Alex Kovalevych <alexkovalevych@gmail.com>
2018-05-31 16:19:58 +0300

    [minor] fix printout_content column type

---
+++ apps/ehealth/priv/repo/migrations/20180531131737_set_contract_request_printout_content_text.exs
@@ -0,0 +1,11 @@
+defmodule EHealth.Repo.Migrations.SetContractRequestPrintoutContentText do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  def change do
+    alter table(:contract_requests) do
+      modify(:printout_content, :text)
+    end
+  end
+end
