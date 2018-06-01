50b5c38328ea70726778c8ace8804bebd3a0283f
Alex Kovalevych <alexkovalevych@gmail.com>
2018-06-01 14:34:02 +0300

    [minor] fix getting signed content

--- apps/ehealth/lib/ehealth/declarations/api.ex
+++ apps/ehealth/lib/ehealth/declarations/api.ex
@@ -155,7 +155,7 @@ defmodule EHealth.Declarations.API do
              headers
            ),
          {:ok, secret_url} <- Map.fetch(data, "secret_url"),
-         {:ok, %{"body" => signed_content}} <- @media_storage_api.get_signed_content(secret_url, headers),
+         {:ok, %{body: signed_content}} <- @media_storage_api.get_signed_content(secret_url, headers),
          {:ok, %{"data" => %{"content" => content}}} <-
            @signature_api.decode_and_validate(Base.encode64(signed_content), "base64", headers) do
       Map.put(declaration_data, "content", content)
