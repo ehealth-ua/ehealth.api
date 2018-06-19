2e69d550d1ee91c9d7f8c2d1a3a5132bd365cef6
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-06-19 15:52:09 +0300

    [minor] fix external_contractors field type (#2489)

---
+++ apps/ehealth/priv/prm_repo/migrations/20180619120617_fix_external_contractors.exs
@@ -0,0 +1,11 @@
+defmodule EHealth.PRMRepo.Migrations.FixExternalContractors do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  def change do
+    execute("""
+    ALTER TABLE contracts ALTER COLUMN external_contractors TYPE JSONB USING (to_json(external_contractors))
+    """)
+  end
+end
