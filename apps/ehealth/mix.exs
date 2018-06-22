7c65f3075566d4ca5fda1ef1f9779824598de5a5
Dmitry Getmantsov <mgck@tuta.io>
2018-06-22 14:32:25 +0300

    [minor] Suppress Plug 1.5.1 warnings (#2512)

--- apps/ehealth/mix.exs
+++ apps/ehealth/mix.exs
@@ -56,7 +56,7 @@ defmodule EHealth.Mixfile do
       {:ecto, "~> 2.1"},
       {:scrivener_ecto, "~> 1.2"},
       {:ecto_trail, "0.2.3"},
-      {:phoenix, "~> 1.3.0"},
+      {:phoenix, "~> 1.3.3"},
       {:phoenix_ecto, "~> 3.2"},
       {:eview, "~> 0.12.2"},
       {:jvalid, "~> 0.6.0"},
