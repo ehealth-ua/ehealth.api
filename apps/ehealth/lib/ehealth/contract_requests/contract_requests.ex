cd29441c48083fdba7e26baf06bd09846a95b7be
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-07-06 18:22:15 +0300

    [minor] fix nhs_signed_date for printout form (#2612)

--- apps/ehealth/lib/ehealth/contract_requests/contract_requests.ex
+++ apps/ehealth/lib/ehealth/contract_requests/contract_requests.ex
@@ -502,7 +502,7 @@ defmodule EHealth.ContractRequests do
              "Incorrect status of contract_request to generate printout form"
            ),
          {:ok, printout_content} <-
-           ContractRequestPrintoutForm.render(Map.put(contract_request, "nhs_signed_date", Date.utc_today()), headers) do
+           ContractRequestPrintoutForm.render(Map.put(contract_request, :nhs_signed_date, Date.utc_today()), headers) do
       {:ok, contract_request, printout_content}
     end
   end
@@ -666,7 +666,7 @@ defmodule EHealth.ContractRequests do
   end
 
   def nhs_signed_changeset(%ContractRequest{} = contract_request, params) do
-    fields = ~w(status updated_by printout_content)a
+    fields = ~w(status updated_by printout_content nhs_signed_date)a
 
     contract_request
     |> cast(params, fields)
