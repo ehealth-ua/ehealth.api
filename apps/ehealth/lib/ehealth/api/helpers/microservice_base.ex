5552c22c4b78063f4cce292144168da9ed846fb4
imax-iva <34630609+imax-iva@users.noreply.github.com>
2018-07-13 15:35:59 +0300

    [minor] microservice_base request params validation fixed (#2664)

--- apps/ehealth/lib/ehealth/api/helpers/microservice_base.ex
+++ apps/ehealth/lib/ehealth/api/helpers/microservice_base.ex
@@ -21,42 +21,79 @@ defmodule EHealth.API.Helpers.MicroserviceBase do
       end
 
       def request!(method, url, body \\ "", headers \\ [], options \\ []) do
-        response = super(method, url, body, headers, options)
+        with {:ok, _} <- check_params(options) do
+          response = super(method, url, body, headers, options)
 
-        if response.status_code >= 300 do
-          Logger.error(fn ->
-            Jason.encode!(%{
-              "log_type" => "microservice_response",
-              "microservice" => config()[:endpoint],
-              "response" => body,
-              "request_id" => Logger.metadata()[:request_id]
-            })
-          end)
+          if response.status_code >= 300 do
+            Logger.error(fn ->
+              Jason.encode!(%{
+                "log_type" => "microservice_response",
+                "microservice" => config()[:endpoint],
+                "response" => body,
+                "request_id" => Logger.metadata()[:request_id]
+              })
+            end)
+          end
+
+          ResponseDecoder.check_response(response)
         end
-
-        ResponseDecoder.check_response(response)
       end
 
       def request(method, url, body \\ "", headers \\ [], options \\ []) do
+        with {:ok, params} <- check_params(options) do
+          query_string = if Enum.empty?(params), do: "", else: "?#{URI.encode_query(params)}"
+
+          Logger.info(fn ->
+            Jason.encode!(%{
+              "log_type" => "microservice_request",
+              "microservice" => config()[:endpoint],
+              "action" => method,
+              "path" => Enum.join([process_url(url), query_string]),
+              "request_id" => Logger.metadata()[:request_id],
+              "body" => body,
+              "headers" =>
+                Enum.reduce(process_request_headers(headers), %{}, fn {k, v}, map ->
+                  Map.put_new(map, k, v)
+                end)
+            })
+          end)
+
+          super(method, url, body, headers, options)
+        end
+      end
+
+      defp check_params(options) do
         params = Keyword.get(options, :params, [])
-        query_string = if Enum.empty?(params), do: "", else: "?#{URI.encode_query(params)}"
 
-        Logger.info(fn ->
-          Jason.encode!(%{
-            "log_type" => "microservice_request",
-            "microservice" => config()[:endpoint],
-            "action" => method,
-            "path" => Enum.join([process_url(url), query_string]),
-            "request_id" => Logger.metadata()[:request_id],
-            "body" => body,
-            "headers" =>
-              Enum.reduce(process_request_headers(headers), %{}, fn {k, v}, map ->
-                Map.put_new(map, k, v)
-              end)
-          })
-        end)
+        errors =
+          Enum.reduce(params, [], fn
+            {k, v}, errors_list when is_list(v) ->
+              errors_list ++ error_description(k)
 
-        super(method, url, body, headers, options)
+            {k, v}, errors_list ->
+              try do
+                to_string(v)
+                errors_list
+              rescue
+                error ->
+                  errors_list ++ error_description(k)
+              end
+          end)
+
+        if length(errors) > 0, do: {:error, errors}, else: {:ok, params}
+      end
+
+      defp error_description(value_name) do
+        [
+          {
+            %{
+              description: "Request parameter #{value_name} is not valid",
+              params: [],
+              rule: :invalid
+            },
+            "$.#{value_name}"
+          }
+        ]
       end
     end
   end
--- apps/ehealth/test/web/controllers/user_role_controller_test.exs
+++ apps/ehealth/test/web/controllers/user_role_controller_test.exs
@@ -5,6 +5,10 @@ defmodule EHealth.Web.UserRoleControllerTest do
   import Mox
   alias Ecto.UUID
 
+  defmodule MicroserviceBaseTest do
+    use EHealth.API.Helpers.MicroserviceBase
+  end
+
   test "get current user roles", %{conn: conn} do
     user_id = UUID.generate()
     client_id = UUID.generate()
@@ -21,5 +25,39 @@ defmodule EHealth.Web.UserRoleControllerTest do
 
     conn = get(conn, user_role_path(conn, :index, client_id: client_id))
     assert [%{"user_id" => ^user_id, "client_id" => ^client_id}] = json_response(conn, 200)["data"]
+  end
+
+  test "invalid request params" do
+    error =
+      {:error,
+       [
+         {%{
+            description: "Request parameter client_id is not valid",
+            params: [],
+            rule: :invalid
+          }, "$.client_id"}
+       ]}
+
+    options = [
+      params: %{
+        "client_id" => %{
+          "0" => "7e9cffd9-c75f-45fb-badf-6e8d20b6a8a8",
+          "1" => "7e9cffd9-c75f-45fb-badf-6e8d20b6a8a8"
+        }
+      }
+    ]
+
+    assert MicroserviceBaseTest.request(:get, "", "", [], options) == error
+
+    options = [
+      params: %{
+        "client_id" => [
+          "7e9cffd9-c75f-45fb-badf-6e8d20b6a8a8",
+          "7e9cffd9-c75f-45fb-badf-6e8d20b6a8a8"
+        ]
+      }
+    ]
+
+    assert MicroserviceBaseTest.request(:get, "", "", [], options) == error
   end
 end
