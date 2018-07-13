c1cd69641cb3f0543a106c6e22ccc98b5dc69e45
Marina Victoria Krasnova <marinakrasnovatr81@gmail.com>
2018-07-13 11:50:48 +0300

    [minor] Declaration request termination (#2663)

---
+++ apps/ehealth/priv/repo/migrations/20180712192600_declaration_requests_change_index.exs
@@ -0,0 +1,19 @@
+defmodule EHealth.Repo.Migrations.DeclarationRequestsChangeIndex do
+  use Ecto.Migration
+
+  @disable_ddl_transaction true
+  def change do
+    execute(
+      "CREATE INDEX CONCURRENTLY IF NOT EXISTS declaration_requests_inserted_at_status_index ON declaration_requests(inserted_at desc, status);"
+    )
+
+    execute("DROP INDEX CONCURRENTLY IF EXISTS declaration_requests_status_inserted_at_index;")
+
+    execute("DROP INDEX CONCURRENTLY IF EXISTS data_legal_entity_id_index;")
+
+    execute("""
+    CREATE INDEX CONCURRENTLY IF NOT EXISTS data_legal_entity_id_inserted_at_index
+    ON public.declaration_requests (cast(data->'legal_entity'->>'id' as text), inserted_at desc);
+    """)
+  end
+end
