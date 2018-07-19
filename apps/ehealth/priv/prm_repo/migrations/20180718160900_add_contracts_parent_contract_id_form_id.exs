27e4413ec15cc410fe609f65b9cc860a98b7230b
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-07-19 11:32:11 +0300

    fix migration (#2738)

--- apps/ehealth/priv/prm_repo/migrations/20180718160900_add_contracts_parent_contract_id_form_id.exs
+++ apps/ehealth/priv/prm_repo/migrations/20180718160900_add_contracts_parent_contract_id_form_id.exs
@@ -1,10 +1,12 @@
 defmodule EHealth.PRMRepo.Migrations.AddContractsParentIdFormId do
+  @moduledoc false
+
   use Ecto.Migration
 
   def change do
     alter table(:contracts) do
       add(:parent_contract_id, :uuid)
-      add(:id_form, :string, null: false)
+      add(:id_form, :string, null: false, default: "PMD_1")
       add(:nhs_signed_date, :date, null: false)
     end
   end
