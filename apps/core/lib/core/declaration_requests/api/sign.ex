0778f4c8ef2cd8789115c1b0706d84f548427a7f
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-08-30 18:26:11 +0300

    Fix builds by disabling casher mock (#3040)

--- apps/core/lib/core/declaration_requests/api/sign.ex
+++ apps/core/lib/core/declaration_requests/api/sign.ex
@@ -257,7 +257,8 @@ defmodule Core.DeclarationRequests.API.Sign do
     |> @ops_api.create_declaration_with_termination_logic(headers)
   end
 
-  defp update_casher_person_data(employee_id) do
+  defp update_casher_person_data(_employee_id) do
+    # todo: uncomment after deploy
     # with {:ok, _response} <- @casher_api.update_person_data(%{"employee_id" => employee_id}, []) do
     :ok
     # end
