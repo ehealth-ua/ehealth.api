634e03117335a1e40ed8e6ee30edbdd43497942b
Pavel Vesnin <pavel.vesnin.v@gmail.com>
2018-10-18 12:26:17 +0300

    GraphQL: negative tests (#3286)
    * GraphQL: negative tests
    
    * GraphQL: changed string param to atom, typo
    
    * GraphQL: use pattern matching instead of case
    
    * GraphQL: format

--- apps/core/lib/core/jobs/jobs.ex
+++ apps/core/lib/core/jobs/jobs.ex
@@ -13,6 +13,9 @@ defmodule Core.Jobs do
   alias Core.Validators.JsonSchema
   alias Core.Validators.Signature
 
+  @status_active LegalEntity.status(:active)
+  @type_msp LegalEntity.type(:msp)
+
   def create_merge_legal_entities_job(%{signed_content: %{content: content, encoding: encoding}}, headers) do
     user_id = get_consumer_id(headers)
 
@@ -20,8 +23,8 @@ defmodule Core.Jobs do
          :ok <- Signature.check_drfo(signer, user_id, "merge_legal_entities"),
          :ok <- JsonSchema.validate(:legal_entity_merge_job, content),
          :ok <- validate_merged_id(content["merged_from_legal_entity"]["id"], content["merged_to_legal_entity"]["id"]),
-         :ok <- validate_related_legal_entity("from", content),
-         :ok <- validate_related_legal_entity("to", content),
+         :ok <- validate_is_merged(:from, content),
+         :ok <- validate_is_merged(:to, content),
          {:ok, legal_entity_from} <- validate_legal_entity("from", content),
          {:ok, legal_entity_to} <- validate_legal_entity("to", content),
          :ok <- validate_legal_entities_type(legal_entity_from, legal_entity_to) do
@@ -32,14 +35,25 @@ defmodule Core.Jobs do
     end
   end
 
-  defp validate_related_legal_entity(direction, content) do
-    where = [
-      is_active: true,
-      "merged_#{direction}_id": content["merged_#{direction}_legal_entity"]["id"]
-    ]
+  defp validate_is_merged(:to, content),
+    do:
+      validate_related_legal_entity(
+        content["merged_to_legal_entity"]["id"],
+        "Merged to legal entity is in the process of reorganization itself"
+      )
+
+  defp validate_is_merged(:from, content),
+    do:
+      validate_related_legal_entity(
+        content["merged_from_legal_entity"]["id"],
+        "Merged from legal entity is already in the process of reorganization"
+      )
+
+  defp validate_related_legal_entity(id, message) do
+    where = [merged_from_id: id, is_active: true]
 
     case LegalEntities.get_related_by(where) do
-      %RelatedLegalEntity{} -> {:error, "Merged #{direction} legal entity is in the process of reorganisation itself"}
+      %RelatedLegalEntity{} -> {:error, message}
       _ -> :ok
     end
   end
@@ -48,9 +62,10 @@ defmodule Core.Jobs do
     %{"id" => id, "name" => name, "edrpou" => edrpou} = content["merged_#{direction}_legal_entity"]
 
     with {:ok, legal_entity} <- validate_is_active(direction, id),
-         :ok <- validate_processed(direction, id),
          :ok <- validate_name(direction, legal_entity, name),
-         :ok <- validate_edrpou(direction, legal_entity, edrpou) do
+         :ok <- validate_edrpou(direction, legal_entity, edrpou),
+         :ok <- validate_status(direction, legal_entity),
+         :ok <- validate_processed(direction, id) do
       {:ok, legal_entity}
     end
   end
@@ -81,11 +96,14 @@ defmodule Core.Jobs do
   defp validate_edrpou(_direction, %{edrpou: edrpou}, request_edrpou) when edrpou == request_edrpou, do: :ok
   defp validate_edrpou(direction, _, _), do: {:error, "Invalid merged #{direction} legal entity edrpou"}
 
+  defp validate_status(_direction, %{status: @status_active}), do: :ok
+  defp validate_status(direction, _), do: {:error, "Merged #{direction} legal entity must be active"}
+
   defp validate_merged_id(from_id, to_id) when from_id != to_id, do: :ok
   defp validate_merged_id(_, _), do: {:error, "Legator and successor of legal entities must be different"}
 
-  defp validate_legal_entities_type(%{type: type_from}, %{type: type_to}) when type_from == type_to, do: :ok
-  defp validate_legal_entities_type(_, _), do: {:error, "Legal entity types should be identical"}
+  defp validate_legal_entities_type(%{type: @type_msp}, %{type: @type_msp}), do: :ok
+  defp validate_legal_entities_type(_, _), do: {:error, "Invalid legal entity type"}
 
   defp create(content, headers) do
     meta = Map.take(content, ~w(merged_from_legal_entity merged_to_legal_entity))
--- apps/graphql/test/web/resolvers/legal_entity_merge_job_resolver_test.exs
+++ apps/graphql/test/web/resolvers/legal_entity_merge_job_resolver_test.exs
@@ -8,6 +8,7 @@ defmodule GraphQLWeb.LegalEntityMergeJobResolverTest do
 
   alias Absinthe.Relay.Node
   alias BSON.ObjectId
+  alias Core.LegalEntities.LegalEntity
   alias Ecto.UUID
   alias TasKafka.Jobs
 
@@ -29,73 +30,30 @@ defmodule GraphQLWeb.LegalEntityMergeJobResolverTest do
   setup %{conn: conn} do
     consumer_id = UUID.generate()
 
+    party = insert(:prm, :party, tax_id: @tax_id)
+    insert(:prm, :party_user, party: party, user_id: consumer_id)
+
     conn =
       conn
       |> put_scope("legal_entity:merge")
       |> put_drfo(@tax_id)
       |> put_consumer_id(consumer_id)
 
-    {:ok, %{conn: conn, consumer_id: consumer_id}}
+    {:ok, %{conn: conn}}
   end
 
   describe "merge legal entities" do
-    test "success", %{conn: conn, consumer_id: consumer_id} do
-      expect(SignatureMock, :decode_and_validate, fn signed_content, :base64, _headers ->
-        content = signed_content |> Base.decode64!() |> Jason.decode!()
-
-        data = %{
-          "content" => content,
-          "signatures" => [
-            %{
-              "is_valid" => true,
-              "signer" => %{
-                "drfo" => @tax_id,
-                "surname" => content["last_name"],
-                "given_name" => "Сара Коннор"
-              },
-              "validation_error_message" => ""
-            }
-          ]
-        }
-
-        {:ok, %{"data" => data}}
-      end)
-
-      party = insert(:prm, :party, tax_id: @tax_id)
-      insert(:prm, :party_user, party: party, user_id: consumer_id)
+    test "success", %{conn: conn} do
+      expect_decode_and_validate()
 
       from = insert(:prm, :legal_entity)
       to = insert(:prm, :legal_entity)
 
-      signed_content =
-        %{
-          "merged_from_legal_entity" => %{
-            "id" => from.id,
-            "name" => from.name,
-            "edrpou" => from.edrpou
-          },
-          "merged_to_legal_entity" => %{
-            "id" => to.id,
-            "name" => to.name,
-            "edrpou" => to.edrpou
-          },
-          "reason" => "Because I can"
-        }
-        |> Jason.encode!()
-        |> Base.encode64()
-
-      variables = %{
-        input: %{
-          signedContent: %{
-            content: signed_content,
-            encoding: "BASE64"
-          }
-        }
-      }
+      signed_content = merged_signed_content(from, to)
 
       job =
         conn
-        |> post_query(@query, variables)
+        |> post_query(@query, input_signed_content(signed_content))
         |> json_response(200)
         |> get_in(~w(data mergeLegalEntities legalEntityMergeJob))
 
@@ -106,18 +64,107 @@ defmodule GraphQLWeb.LegalEntityMergeJobResolverTest do
       resp =
         conn
         |> put_scope("scope:not-allowed")
-        |> post_query(@query, %{
-          input: %{
-            signedContent: %{
-              content: "",
-              encoding: "BASE64"
-            }
-          }
-        })
+        |> post_query(@query, input_signed_content(""))
         |> json_response(200)
 
       assert match?(%{"mergeLegalEntities" => nil}, resp["data"])
       assert Enum.any?(resp["errors"], &match?(%{"extensions" => %{"code" => "FORBIDDEN"}}, &1))
+    end
+
+    test "merged from legal entity with status CLOSED", %{conn: conn} do
+      expect_decode_and_validate()
+
+      from = insert(:prm, :legal_entity, status: LegalEntity.status(:closed))
+      to = insert(:prm, :legal_entity)
+
+      signed_content = merged_signed_content(from, to)
+
+      resp =
+        conn
+        |> post_query(@query, input_signed_content(signed_content))
+        |> json_response(200)
+
+      assert match?(%{"mergeLegalEntities" => nil}, resp["data"])
+      assert Enum.any?(resp["errors"], &match?(%{"message" => "Merged from legal entity must be active"}, &1))
+    end
+
+    test "merged to legal entity with status CLOSED", %{conn: conn} do
+      expect_decode_and_validate()
+
+      from = insert(:prm, :legal_entity)
+      to = insert(:prm, :legal_entity, status: LegalEntity.status(:closed))
+
+      signed_content = merged_signed_content(from, to)
+
+      resp =
+        conn
+        |> post_query(@query, input_signed_content(signed_content))
+        |> json_response(200)
+
+      assert match?(%{"mergeLegalEntities" => nil}, resp["data"])
+      assert Enum.any?(resp["errors"], &match?(%{"message" => "Merged to legal entity must be active"}, &1))
+    end
+
+    test "merge with invalid legal entity type", %{conn: conn} do
+      expect_decode_and_validate(2)
+
+      from = insert(:prm, :legal_entity)
+      to1 = insert(:prm, :legal_entity, type: LegalEntity.type(:mis))
+      to2 = insert(:prm, :legal_entity, type: LegalEntity.type(:pharmacy))
+
+      Enum.each([to1, to2], fn to ->
+        resp =
+          conn
+          |> post_query(@query, input_signed_content(merged_signed_content(from, to)))
+          |> json_response(200)
+
+        assert match?(%{"mergeLegalEntities" => nil}, resp["data"])
+        assert Enum.any?(resp["errors"], &match?(%{"message" => "Invalid legal entity type"}, &1))
+      end)
+    end
+
+    test "merged to already merged", %{conn: conn} do
+      expect_decode_and_validate()
+
+      from = insert(:prm, :legal_entity)
+      to = insert(:prm, :legal_entity)
+      insert(:prm, :related_legal_entity, merged_from: to)
+
+      signed_content = merged_signed_content(from, to)
+
+      resp =
+        conn
+        |> post_query(@query, input_signed_content(signed_content))
+        |> json_response(200)
+
+      assert match?(%{"mergeLegalEntities" => nil}, resp["data"])
+
+      assert Enum.any?(
+               resp["errors"],
+               &match?(%{"message" => "Merged to legal entity is in the process of reorganization itself"}, &1)
+             )
+    end
+
+    test "merged from already merged", %{conn: conn} do
+      expect_decode_and_validate()
+
+      from = insert(:prm, :legal_entity)
+      to = insert(:prm, :legal_entity)
+      insert(:prm, :related_legal_entity, merged_from: from)
+
+      signed_content = merged_signed_content(from, to)
+
+      resp =
+        conn
+        |> post_query(@query, input_signed_content(signed_content))
+        |> json_response(200)
+
+      assert match?(%{"mergeLegalEntities" => nil}, resp["data"])
+
+      assert Enum.any?(
+               resp["errors"],
+               &match?(%{"message" => "Merged from legal entity is already in the process of reorganization"}, &1)
+             )
     end
   end
 
@@ -185,5 +232,57 @@ defmodule GraphQLWeb.LegalEntityMergeJobResolverTest do
   defp create_job(meta) do
     {:ok, job} = Jobs.create(meta)
     {:ok, ObjectId.encode!(job._id), job}
+  end
+
+  defp merged_signed_content(merged_from, merged_to) do
+    %{
+      "merged_from_legal_entity" => %{
+        "id" => merged_from.id,
+        "name" => merged_from.name,
+        "edrpou" => merged_from.edrpou
+      },
+      "merged_to_legal_entity" => %{
+        "id" => merged_to.id,
+        "name" => merged_to.name,
+        "edrpou" => merged_to.edrpou
+      },
+      "reason" => "Because I can"
+    }
+    |> Jason.encode!()
+    |> Base.encode64()
+  end
+
+  defp expect_decode_and_validate(n \\ 1) do
+    expect(SignatureMock, :decode_and_validate, n, fn signed_content, :base64, _headers ->
+      content = signed_content |> Base.decode64!() |> Jason.decode!()
+
+      data = %{
+        "content" => content,
+        "signatures" => [
+          %{
+            "is_valid" => true,
+            "signer" => %{
+              "drfo" => @tax_id,
+              "surname" => content["last_name"],
+              "given_name" => "Сара Коннор"
+            },
+            "validation_error_message" => ""
+          }
+        ]
+      }
+
+      {:ok, %{"data" => data}}
+    end)
+  end
+
+  defp input_signed_content(content) do
+    %{
+      input: %{
+        signedContent: %{
+          content: content,
+          encoding: "BASE64"
+        }
+      }
+    }
   end
 end
