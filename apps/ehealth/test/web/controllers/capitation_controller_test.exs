16799e8ecd87a07ef9ae5c7a7075aed8b8fb3227
imax-iva <34630609+imax-iva@users.noreply.github.com>
2018-11-01 18:15:24 +0200

    capitation view fixed (#3395)

--- apps/ehealth/test/web/controllers/capitation_controller_test.exs
+++ apps/ehealth/test/web/controllers/capitation_controller_test.exs
@@ -37,8 +37,10 @@ defmodule EHealth.Web.CapitationControllerTest do
 
       assert %{"data" => [%{"id" => ^report_id}], "paging" => %{"page_number" => 2, "page_size" => 1}} = response
     end
+  end
 
-    test "List capitation report details", %{conn: conn} do
+  describe "capitation reports details" do
+    test "capitation report details success", %{conn: conn} do
       nhs(3)
       report_id = UUID.generate()
 
@@ -156,6 +158,81 @@ defmodule EHealth.Web.CapitationControllerTest do
       assert %{"data" => data} = response
 
       check_response.(data)
+    end
+
+    test "capitation report details succes when edrpou is nil", %{conn: conn} do
+      nhs()
+
+      expect(ReportMock, :get_capitation_details, 3, fn _params, _headers ->
+        {:ok,
+         %{
+           "data" => [
+             %{
+               "billing_date" => "2018-06-01",
+               "capitation_contracts" => [
+                 %{
+                   "contract_id" => "790778e6-b4ad-4396-992d-d85ca6dd9365",
+                   "contract_number" => "contract-number-4381",
+                   "details" => [
+                     %{
+                       "attributes" => [%{"40-60" => 2}, %{"19-39" => 2}, %{"0-18" => 1}],
+                       "mountain_group" => true
+                     },
+                     %{
+                       "attributes" => [%{"19-39" => 4}, %{"0-18" => 3}, %{"40-60" => 4}],
+                       "mountain_group" => false
+                     }
+                   ],
+                   "total" => [%{"19-39" => 6}, %{"0-18" => 4}, %{"40-60" => 6}]
+                 },
+                 %{
+                   "contract_id" => "0b13a1b2-007d-48f9-9af7-302205ff1523",
+                   "contract_number" => "contract-number-5128",
+                   "details" => [
+                     %{
+                       "attributes" => [%{"0-18" => 4}, %{"19-39" => 3}, %{"40-60" => 1}],
+                       "mountain_group" => false
+                     },
+                     %{
+                       "attributes" => [%{"40-60" => 1}, %{"19-39" => 3}, %{"0-18" => 2}],
+                       "mountain_group" => true
+                     }
+                   ],
+                   "total" => [%{"0-18" => 6}, %{"19-39" => 6}, %{"40-60" => 2}]
+                 },
+                 %{
+                   "contract_id" => "77f5e5ae-7e77-468d-b32e-f3a50c21c4f9",
+                   "contract_number" => "contract-number-7395",
+                   "details" => [
+                     %{
+                       "attributes" => [%{"19-39" => 1}, %{"40-60" => 3}, %{"0-18" => 3}],
+                       "mountain_group" => false
+                     },
+                     %{
+                       "attributes" => [%{"19-39" => 1}, %{"0-18" => 1}, %{"40-60" => 2}],
+                       "mountain_group" => true
+                     }
+                   ],
+                   "total" => [%{"0-18" => 4}, %{"40-60" => 5}, %{"19-39" => 2}]
+                 }
+               ],
+               "edrpou" => nil,
+               "legal_entity_id" => "2ec2b749-41d0-461b-aa02-1abd7ccbd86b",
+               "legal_entity_name" => "Blastoise",
+               "report_id" => "9ce44b25-cf35-4629-a5ee-dfed7470daaf"
+             }
+           ],
+           "paging" => %{"page_number" => 2, "page_size" => 1}
+         }}
+      end)
+
+      response =
+        conn
+        |> put_client_id_header(UUID.generate())
+        |> get(capitation_path(conn, :details, %{page_size: 1, page: 2}))
+        |> json_response(200)
+
+      assert %{"data" => _, "paging" => %{"page_number" => 2, "page_size" => 1}} = response
     end
   end
 end
