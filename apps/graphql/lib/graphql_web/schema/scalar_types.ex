60b144840b2bffdf993bac2144db11ed9bc4392f
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-11-08 15:40:30 +0200

    Set division working_hours as json in graphql schema (#3444)
    * Set division working_hours as json in graphql schema
    
    * Update apps/graphql/lib/graphql_web/schema/scalar_types.ex
    
    Co-Authored-By: progsmile <denis.klimenko.dx@gmail.com>
    
    * Update apps/graphql/lib/graphql_web/schema/scalar_types.ex
    
    Co-Authored-By: progsmile <denis.klimenko.dx@gmail.com>
    
    * Update apps/graphql/lib/graphql_web/schema/scalar_types.ex
    
    Co-Authored-By: progsmile <denis.klimenko.dx@gmail.com>
    
    * Update apps/graphql/lib/graphql_web/schema/scalar_types.ex
    
    Co-Authored-By: progsmile <denis.klimenko.dx@gmail.com>
    
    * Remove match on input object in graphql json type

--- apps/graphql/lib/graphql_web/schema/scalar_types.ex
+++ apps/graphql/lib/graphql_web/schema/scalar_types.ex
@@ -10,6 +10,11 @@ defmodule GraphQLWeb.Schema.ScalarTypes do
     parse(&parse_date_interval/1)
   end
 
+  scalar :json do
+    serialize(& &1)
+    parse(&decode_json/1)
+  end
+
   defp parse_date_interval(%Input.String{value: value}) do
     case Date.Interval.from_edtf(value) do
       {:ok, interval} -> {:ok, interval}
@@ -19,4 +24,14 @@ defmodule GraphQLWeb.Schema.ScalarTypes do
 
   defp parse_date_interval(%Input.Null{}), do: {:ok, nil}
   defp parse_date_interval(_), do: :error
+
+  defp decode_json(%Input.String{value: value}) do
+    case Jason.decode(value) do
+      {:ok, result} -> {:ok, result}
+      _ -> :error
+    end
+  end
+
+  defp decode_json(%Input.Null{}), do: {:ok, nil}
+  defp decode_json(_), do: :error
 end
