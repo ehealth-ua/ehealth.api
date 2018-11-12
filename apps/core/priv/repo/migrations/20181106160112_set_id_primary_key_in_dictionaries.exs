2210c21a7487f7ac041af8179b28600e47fae80a
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-11-12 16:26:18 +0200

    Fix dictionary migration to set id (#3504)

--- apps/core/priv/repo/migrations/20181106160112_set_id_primary_key_in_dictionaries.exs
+++ apps/core/priv/repo/migrations/20181106160112_set_id_primary_key_in_dictionaries.exs
@@ -1,10 +1,16 @@
 defmodule Core.Repo.Migrations.SetIdPrimaryKeyInDictionaries do
   use Ecto.Migration
 
+  alias Core.Dictionaries.Dictionary
+  alias Core.Repo
+  alias Ecto.UUID
+
   @disable_ddl_transaction true
 
   def change do
-    Core.Repo.update_all(Core.Dictionaries.Dictionary, [set: [id: Ecto.UUID.generate()]])
+    Dictionary
+    |> Repo.all()
+    |> Enum.each(&execute("UPDATE dictionaries SET id = '#{UUID.generate()}' WHERE name='#{&1.name}';"))
 
     execute("ALTER TABLE dictionaries DROP CONSTRAINT dictionaries_pkey")
     create unique_index(:dictionaries, :name, name: :dictionaries_name_uniq)
