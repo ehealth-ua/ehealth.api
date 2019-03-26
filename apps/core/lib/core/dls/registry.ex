06236d425c1ad08db57a0908ebf766239f3b865e
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-03-26 11:57:31 +0200

    feat: dls registry table (#4550)

---
+++ apps/core/lib/core/dls/registry.ex
@@ -0,0 +1,17 @@
+defmodule Core.DLS.Registry do
+  @moduledoc false
+
+  use Ecto.Schema
+  alias Core.Divisions.Division
+  alias Ecto.UUID
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  schema "dls_registry" do
+    field(:dls_id, :string)
+    field(:dls_status, :string)
+
+    belongs_to(:division, Division, type: UUID)
+
+    timestamps(type: :utc_datetime, updated_at: false)
+  end
+end
---
+++ apps/core/priv/prm_repo/migrations/20190325152945_dls_registry.exs
@@ -0,0 +1,16 @@
+defmodule Core.PRMRepo.Migrations.DlsRegistry do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  def change do
+    create table("dls_registry", primary_key: false) do
+      add(:id, :uuid, primary_key: true)
+      add(:dls_id, :string)
+      add(:dls_status, :string)
+      add(:division_id, references("divisions", type: :uuid))
+
+      timestamps(type: :utc_datetime, updated_at: false)
+    end
+  end
+end
