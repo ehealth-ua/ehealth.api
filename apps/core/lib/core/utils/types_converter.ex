ee34784aa09005a8306607c55267b7c4f07162d1
Denis Klimenko <denis.klimenko.dx@gmail.com>
2019-05-30 12:32:28 +0300

    fix: match error on convert types (#5107)

--- apps/core/lib/core/utils/types_converter.ex
+++ apps/core/lib/core/utils/types_converter.ex
@@ -18,6 +18,5 @@ defmodule Core.Utils.TypesConverter do
   def string_to_integer(string) when is_binary(string), do: String.to_integer(string)
   def string_to_integer(string), do: string
 
-  def atoms_to_strings(%{} = map), do: map |> Jason.encode!() |> Jason.decode!()
-  def atoms_to_strings(val) when is_list(val), do: Enum.map(val, &atoms_to_strings/1)
+  def atoms_to_strings(value), do: value |> Jason.encode!() |> Jason.decode!()
 end
