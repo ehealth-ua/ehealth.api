80d18e766124ce5ac0e78e1af2f8746b2a67aa76
AlekzandrV <sp.virny@gmail.com>
2019-05-31 13:56:23 +0300

    chore: Added JSON schemas for new employee types (#5145)

--- apps/core/lib/core/validators/json_schema.ex
+++ apps/core/lib/core/validators/json_schema.ex
@@ -72,6 +72,8 @@ defmodule Core.Validators.JsonSchema do
   use_schema(:employee_request_sign, "specs/json_schemas/employee_request/employee_request_sign.json")
   use_schema(:employee_doctor, "specs/json_schemas/employee/employee_doctor_create_request.json")
   use_schema(:employee_pharmacist, "specs/json_schemas/employee/employee_pharmacist_create_request.json")
+  use_schema(:employee_surgeon, "specs/json_schemas/employee/employee_surgeon_create_request.json")
+  use_schema(:employee_specialist, "specs/json_schemas/employee/employee_specialist_create_request.json")
   use_schema(:legal_entity_v2, "specs/json_schemas/legal_entity/v2/legal_entity_create_request.json")
   use_schema(:legal_entity, "specs/json_schemas/legal_entity/legal_entity_create_request.json")
   use_schema(:legal_entity_sign, "specs/json_schemas/legal_entity/legal_entity_sign.json")
