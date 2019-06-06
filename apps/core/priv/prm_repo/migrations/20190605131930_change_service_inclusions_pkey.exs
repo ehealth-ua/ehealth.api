b8155dc2980429bf24e6c250ddb523faa0b8cd70
Myroslav Kiurchev <kiurchv@gmail.com>
2019-06-06 09:07:39 +0000

    fix: correct `ChangeServiceInclusionsPkey` migration (#5194)

--- apps/core/priv/prm_repo/migrations/20190605131930_change_service_inclusions_pkey.exs
+++ apps/core/priv/prm_repo/migrations/20190605131930_change_service_inclusions_pkey.exs
@@ -1,7 +1,15 @@
 defmodule Core.PRMRepo.Migrations.ChangeServiceInclusionsPkey do
   use Ecto.Migration
 
+  def after_begin do
+    execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+  end
+
   def change do
+    alter table(:service_inclusions) do
+      add(:id, :uuid, default: fragment("uuid_generate_v4()"))
+    end
+
     execute(
       "ALTER TABLE service_inclusions DROP CONSTRAINT service_inclusions_pkey",
       "ALTER TABLE service_inclusions ADD PRIMARY KEY (service_group_id, service_id)"
@@ -9,8 +17,14 @@ defmodule Core.PRMRepo.Migrations.ChangeServiceInclusionsPkey do
 
     create unique_index(:service_inclusions, [:service_group_id, :service_id], where: "is_active = true")
 
-    alter table(:service_inclusions) do
-      add(:id, :uuid, primary_key: true)
-    end
+    execute(
+      "ALTER TABLE service_inclusions ADD PRIMARY KEY (id)",
+      "ALTER TABLE service_inclusions DROP CONSTRAINT service_inclusions_pkey"
+    )
+
+    execute(
+      "ALTER TABLE service_inclusions ALTER COLUMN id DROP DEFAULT",
+      "ALTER TABLE service_inclusions ALTER COLUMN id SET DEFAULT uuid_generate_v4()"
+    )
   end
 end
