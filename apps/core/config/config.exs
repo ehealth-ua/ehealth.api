2da9d7a3247d6511db4dbce8a13fed65946ef864
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-06-11 12:48:00 +0300

    chore: add rpc timeout config (#5234)

--- apps/core/config/config.exs
+++ apps/core/config/config.exs
@@ -376,7 +376,10 @@ config :cipher,
   keyphrase: System.get_env("CIPHER_KEYPHRASE") || "8()VN#U#_CU#X)*BFG(Cadsvn$&",
   ivphrase: System.get_env("CIPHER_IVPHRASE") || "B((%(^(%V(CWBY(**(by(*YCBDYB#(Y(C#"
 
-config :core, Core.Rpc.Worker, max_attempts: {:system, :integer, "RPC_MAX_ATTEMPTS", 3}
+config :core, Core.Rpc.Worker,
+  max_attempts: {:system, :integer, "RPC_MAX_ATTEMPTS", 3},
+  timeout: {:system, :integer, "RPC_TIMEOUT", 5_000}
+
 config :core, Core.Rpc.EdrWorker, timeout: {:system, :integer, "RPC_EDR_WORKER_TIMEOUT", 15_000}
 
 config :kaffe,
